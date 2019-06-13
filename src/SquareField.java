2d324447e719c536a18959498fc55dc617089576
Aleksander Weber <aleksander.weber@icloud.com>
2019-06-13 20:33:29 +0200

    test aleks

--- src/SquareField.java
+++ src/SquareField.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class SquareField {
+public class SquareField implements Worker {
+    public void doWork(){
+        System.out.println("Podaj A");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println("Pole kwadratu o boku a="+a + " wynosi: " + (a*a));
+    }
 }

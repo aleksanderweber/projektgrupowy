a79ea95218911beb383d41e5746d2f55901cc9c4
Asia <szelagasia@gmail.com>
2019-06-13 20:53:07 +0200

    Pole koła

--- src/CircleField.java
+++ src/CircleField.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class CircleField {
+public class CircleField implements Worker {
+    public void doWork(){
+        System.out.println("Podaj r");
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+
+
+        System.out.println("Pole koła o promieniu r= "+r +  " wynosi: " + (r*r*Math.PI));
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ public class Main {
     private static Worker workToDo;
 
     public static void main(String[] args) {
-	    printManual();
+        printManual();
         Scanner sc = new Scanner(System.in);
-	    switch (sc.nextInt()){
+        switch (sc.nextInt()) {
             case 1:
                 workToDo = new TriangleField();
+                break;
+            case 2:
+                workToDo = new CircleField();
                 break;
 
             default:
@@ -18,10 +21,12 @@ public class Main {
         workToDo.doWork();
     }
 
-    public static void printManual(){
+    public static void printManual() {
         System.out.println("Wybierz opcje aby wykonać podprogram:");
         System.out.println("1. Liczenie pola trójkąta");
-
+        System.out.println("2. Liczenie pola koła");
         System.out.println("Twój wybór: ");
     }
 }
+
+
